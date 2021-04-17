0cc973f8e07b1343bcbbf546fd0e2706e9960241
David Wendland <david.w.dev@gmail.com>
2021-04-17 22:28:09 +0200

    Add process handler to support application restart

---
+++ SniffCore/ProcessHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+
+namespace SniffCore
+{
+    public static class ProcessHandler
+    {
+        public static void Restart(int delay = 2)
+        {
+            var process = Process.GetCurrentProcess();
+            var module = process.MainModule;
+            if (module == null)
+                return;
+
+            var info = new ProcessStartInfo
+            {
+                Arguments = $"/C ping 127.0.0.1 -n {delay} && \"{module.FileName}\"",
+                WindowStyle = ProcessWindowStyle.Hidden,
+                CreateNoWindow = true,
+                FileName = "cmd.exe"
+            };
+            Process.Start(info);
+            process.Kill();
+        }
+    }
+}

ecc805c35de1a6aa1eb4d0cb85672dbae9572190
David Wendland <the.devicenator@gmail.com>
2021-05-22 11:49:41 +0200

    Add license

--- SniffCore/ObjectEx.cs
+++ SniffCore/ObjectEx.cs
@@ -1,4 +1,9 @@
-﻿namespace SniffCore
+﻿//
+// Copyright (c) David Wendland. All rights reserved.
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+//
+
+namespace SniffCore
 {
     public static class ObjectEx
     {
--- SniffCore/ObservableObject.cs
+++ SniffCore/ObservableObject.cs
@@ -1,4 +1,9 @@
-﻿using System.ComponentModel;
+﻿//
+// Copyright (c) David Wendland. All rights reserved.
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+//
+
+using System.ComponentModel;
 using System.Runtime.CompilerServices;
 
 namespace SniffCore

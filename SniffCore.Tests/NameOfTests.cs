ddad4f21430b0c1def87d2d14761796401127ad2
David Wendland <the.devicenator@gmail.com>
2021-07-17 11:33:24 +0200

    Add NameOf helper object

---
+++ SniffCore.Tests/NameOfTests.cs
@@ -0,0 +1,182 @@
+﻿using NUnit.Framework;
+using SniffCore.Tests.Subnamespace;
+
+namespace SniffCore.Tests
+{
+    [TestFixture]
+    public class NameOfTests
+    {
+        [Test]
+        public void NameT_CalledWithoutProperty_ReturnsSameAsNameof()
+        {
+            var expected = "Second";
+
+            var actual = NameOf.Name<Second>();
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Name_CalledWithoutProperty_ReturnsSameAsNameof()
+        {
+            var expected = "Second";
+
+            var actual = NameOf.Name(typeof(Second));
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void NameT_CalledWithProperty_ReturnsSameAsNameof()
+        {
+            var expected = "Second.Name";
+
+            var actual = NameOf.Name<Second>(nameof(Second.Name));
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Name_CalledWithProperty_ReturnsSameAsNameof()
+        {
+            var expected = "Second.Name";
+            
+            var actual = NameOf.Name(typeof(Second), nameof(Second.Name));
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+        
+        [Test]
+        public void NamespaceT_Called_ReturnsSameAsNameof()
+        {
+            var expected = "SniffCore.Tests.Subnamespace";
+
+            var actual = NameOf.Namespace<Second>();
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Namespace_Called_ReturnsSameAsNameof()
+        {
+            var expected = "SniffCore.Tests.Subnamespace";
+
+            var actual = NameOf.Namespace(typeof(Second));
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void NamespaceT_CalledWithTwoEqualTypes_ReturnsEmpty()
+        {
+            var expected = string.Empty;
+
+            var actual = NameOf.Namespace<First, First>();
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void NamespaceT_CalledWithTwoDifferentTypes_Relatives1()
+        {
+            var expected = "Subnamespace";
+
+            var actual = NameOf.Namespace<First, Second>();
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void NamespaceT_CalledWithTwoDifferentTypes_Relatives2()
+        {
+            var expected = "Subnamespace";
+
+            var actual = NameOf.Namespace<Second, First>();
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Namespace_CalledWithTwoEqualTypes_ReturnsEmpty()
+        {
+            var expected = string.Empty;
+
+            var actual = NameOf.Namespace(typeof(First), typeof(First));
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Namespace_CalledWithTwoDifferentTypes_Relatives1()
+        {
+            var expected = "Subnamespace";
+
+            var actual = NameOf.Namespace(typeof(First), typeof(Second));
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Namespace_CalledWithTwoDifferentTypes_Relatives2()
+        {
+            var expected = "Subnamespace";
+
+            var actual = NameOf.Namespace(typeof(Second), typeof(First));
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+        
+        [Test]
+        public void FullNameT_CalledWithoutProperty_ReturnsSameAsNameof()
+        {
+            var expected = "SniffCore.Tests.Subnamespace.Second";
+
+            var actual = NameOf.FullName<Second>();
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void FullName_CalledWithoutProperty_ReturnsSameAsNameof()
+        {
+            var expected = "SniffCore.Tests.Subnamespace.Second";
+
+            var actual = NameOf.FullName(typeof(Second));
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void FullNameT_CalledWithProperty_ReturnsSameAsNameof()
+        {
+            var expected = "SniffCore.Tests.Subnamespace.Second.Name";
+
+            var actual = NameOf.FullName<Second>(nameof(Second.Name));
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void FullName_CalledWithProperty_ReturnsSameAsNameof()
+        {
+            var expected = "SniffCore.Tests.Subnamespace.Second.Name";
+
+            var actual = NameOf.FullName(typeof(Second), nameof(Second.Name));
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+
+    public class First
+    {
+        public string Name { get; set; }
+    }
+
+    namespace Subnamespace
+    {
+        public class Second
+        {
+            public string Name { get; set; }
+        }
+    }
+}
---
+++ SniffCore/NameOf.cs
@@ -0,0 +1,123 @@
+﻿using System;
+
+namespace SniffCore
+{
+    /// <summary>
+    ///     Provides methods for an easy work with namespace and type names in combination with nameof.
+    /// </summary>
+    public static class NameOf
+    {
+        /// <summary>
+        ///     Returns the name of the given type. Including the property name if given.
+        ///     E.g. "MainViewModel" or "MainViewModel.Name"
+        /// </summary>
+        /// <typeparam name="T">The type which name to read.</typeparam>
+        /// <param name="propertyName">The optional property name to append.</param>
+        /// <returns>The name of the given type. Including the property name if given.</returns>
+        public static string Name<T>(string propertyName = null)
+        {
+            return Name(typeof(T), propertyName);
+        }
+
+        /// <summary>
+        ///     Returns the name of the given type. Including the property name if given.
+        ///     E.g. "MainViewModel" or "MainViewModel.Name"
+        /// </summary>
+        /// <param name="type">The type which name to read.</param>
+        /// <param name="propertyName">The optional property name to append.</param>
+        /// <returns>The name of the given type. Including the property name if given.</returns>
+        public static string Name(Type type, string propertyName = null)
+        {
+            if (!string.IsNullOrWhiteSpace(propertyName))
+                return type.Name + "." + propertyName;
+            return type.Name;
+        }
+
+        /// <summary>
+        ///     Returns the namespace of the given type without the type name itself.
+        ///     E.g. "Application.ViewModels"
+        /// </summary>
+        /// <typeparam name="T">The type which namespace to read.</typeparam>
+        /// <returns>The namespace of the given type without the type name itself.</returns>
+        public static string Namespace<T>()
+        {
+            return Namespace(typeof(T));
+        }
+
+        /// <summary>
+        ///     Returns the namespace of the given type without the type name itself.
+        ///     E.g. "Application.ViewModels"
+        /// </summary>
+        /// <param name="type">The type which namespace to read.</param>
+        /// <returns>The namespace of the given type without the type name itself.</returns>
+        public static string Namespace(Type type)
+        {
+            return type.Namespace;
+        }
+
+        /// <summary>
+        ///     Returns the relative path of one type namespace to the other.
+        ///     E.g. "Application.ViewModels" + "Application.ViewModels.Pages" = "Pages"
+        ///     If both types are in the same namespace or have no namespace in common, the result will be empty.
+        /// </summary>
+        /// <remarks>The order does not matter.</remarks>
+        /// <typeparam name="T1">The first type which namespace to read.</typeparam>
+        /// <typeparam name="T2">The second type which namespace to read.</typeparam>
+        /// <returns>The relative path of one type namespace to the other.</returns>
+        public static string Namespace<T1, T2>()
+        {
+            return Namespace(typeof(T1), typeof(T2));
+        }
+
+        /// <summary>
+        ///     Returns the relative path of one type namespace to the other.
+        ///     E.g. "Application.ViewModels" + "Application.ViewModels.Pages" = "Pages"
+        ///     If both types are in the same namespace or have no namespace in common, the result will be empty.
+        /// </summary>
+        /// <remarks>The order does not matter.</remarks>
+        /// <param name="type1">The first type which namespace to read.</param>
+        /// <param name="type2">The second type which namespace to read.</param>
+        /// <returns>The relative path of one type namespace to the other.</returns>
+        public static string Namespace(Type type1, Type type2)
+        {
+            var first = type1.Namespace;
+            var second = type2.Namespace;
+
+            if (first == null || second == null)
+                return string.Empty;
+            if (first == second)
+                return string.Empty;
+            if (first.StartsWith(second))
+                return first.Substring(second.Length + 1);
+            if (second.StartsWith(first))
+                return second.Substring(first.Length + 1);
+            return string.Empty;
+        }
+
+        /// <summary>
+        ///     Returns the namespace and name of the given type. Including the property name if given.
+        ///     E.g. "Application.ViewModels.MainViewModel" or "Application.ViewModels.MainViewModel.Name"
+        /// </summary>
+        /// <typeparam name="T">The type which namespace and name to read.</typeparam>
+        /// <param name="propertyName">The optional property name to append.</param>
+        /// <returns>The namespace and name of the given type. Including the property name if given.</returns>
+        public static string FullName<T>(string propertyName = null)
+        {
+            return FullName(typeof(T), propertyName);
+        }
+
+        /// <summary>
+        ///     Returns the namespace and name of the given type. Including the property name if given.
+        ///     E.g. "Application.ViewModels.MainViewModel" or "Application.ViewModels.MainViewModel.Name"
+        /// </summary>
+        /// <param name="type">The type which namespace and name to read.</param>
+        /// <param name="propertyName">The optional property name to append.</param>
+        /// <returns>The namespace and name of the given type. Including the property name if given.</returns>
+        public static string FullName(Type type, string propertyName = null)
+        {
+            if (!string.IsNullOrWhiteSpace(propertyName))
+                return type.FullName + "." + propertyName;
+            return type.FullName;
+        }
+    }
+}
